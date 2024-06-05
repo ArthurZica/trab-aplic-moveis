7f52a8fd8173527cd1436366c4725a79bfe50910
Rafael Rosa <79370543+rlrosaa@users.noreply.github.com>
2024-06-04 21:22:40 -0300

    Update NotificacoesScreen.js
    Criação parcial da tela de Notificações

--- src/Pages/NotificacoesScreen.js
+++ src/Pages/NotificacoesScreen.js
@@ -1,18 +1,53 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Image} from 'react-native';
+
+import Container from "../components/Container";
+import Botao from "../components/Botao";
+import Textoh2 from "../components/Textoh2"
+
+const estaticImage = "https://miro.medium.com/v2/resize:fit:720/format:webp/1*g09N-jl7JtVjVZGcd-vL2g.jpeg";
 
 export default function NotificacoesScreen() {
   return (
-    <View style={styles.container}>
-      <Text>Notificações Screen</Text>
-    </View>
+    <Container >
+      <View style={styles.containerInput}>
+        <Botao title="Novos Pedidos"/>
+        <Botao title="Alterações"/>
+      </View>
+      <View style={styles.containerProfile}>
+        <Image
+          style={styles.profileImage}
+          source={{ uri: estaticImage }}
+        />
+        <Textoh2 texto="Usuário"/>
+        <View style= {styles.containerDetails}>
+          <Botao title="Ver Detalhes >" preto/>
+        </View>
+      </View>     
+    </Container>
+    
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+  containerInput: {
+    flexDirection: 'row',    
+    alignItems: 'center', 
+    backgroundColor: '#FFF',    
   },
+  containerProfile: {
+    flexDirection: 'row',    
+    alignItems: 'center', 
+    backgroundColor: '#FFF',
+    padding:10,
+  },
+  containerDetails: {    
+    paddingLeft:30,
+    alignItems: 'flex-end',
+  },
+  profileImage:{
+    width: 64,
+    height: 64,
+    borderRadius: 32,
+  }
 });
